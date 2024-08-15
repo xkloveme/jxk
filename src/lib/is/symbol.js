1207005d6dc90e984383459ac909560ebc5c630a
xkloveme <xkloveme@gmail.com>
2024-08-15 17:30:28 +0800

    fix(is/symbol): 修正symbol别名注释
    修正了`is/symbol.js`模块中`@alias`的注释，将不一致的别名`yd_is_symbol`更新为正确的`is_symbol`。这确保了文档的准确性和一致性，有助于未来的代码维护和可读性。

--- src/lib/is/symbol.js
+++ src/lib/is/symbol.js
@@ -3,7 +3,7 @@ import { getValueType } from '../../utils/common';
  * 判断是否是Symbol值
  * @author xkloveme <xkloveme@gmail.com>
  * @category is
- * @alias yd_is_symbol
+ * @alias is_symbol
  * @param {any} value 任意值
  * @returns {Boolean} 返回是否是Symbol值
  */
