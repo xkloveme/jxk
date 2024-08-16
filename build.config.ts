5a1858278f8e492b9cc76f5bc762817aba0d294c
xkloveme <xkloveme@gmail.com>
2024-08-16 10:57:13 +0800

    ```txt feat(build): 使用unbuild进行构建，并调整vite配置
    引入unbuild以进行库构建，调整vite配置以适应新构建设置。修改package.json以反映构建输出和入口点。更新.build.config.ts以正确配置unbuild选项。调整vite.config.js以适应新的构建需求。
    ```

--- build.config.ts
+++ build.config.ts
@@ -1,17 +1,17 @@
-import { defineBuildConfig } from 'unbuild';
+import { defineBuildConfig } from 'unbuild'
 
 export default defineBuildConfig({
   entries: [
     'src/index',
   ],
-  externals: ['date-fns'], // 外部依赖，如果不需要则为空数组
-  declaration: true, // 生成 .d.ts 文件
-  clean: true, // 构建前清理输出目录
-  outDir: 'dist/types', // 输出目录
+  externals: ['date-fns'],
+  declaration: true,
+  clean: true,
+  failOnWarn: false,
   rollup: {
-    emitCJS: false, // 同时输出 CommonJS 版本
+    emitCJS: true,
     esbuild: {
-      minify: true, // 压缩输出文件
+      minify: true,
     },
   },
-});
+})
--- vite.config.js
+++ vite.config.js
@@ -4,21 +4,13 @@ import { defineConfig } from 'vite'
 
 export default defineConfig({
   build: {
+    outDir: 'lib',
     lib: {
       entry: resolve(__dirname, './src/index.js'),
       name: 'jxk',
       fileName: 'index',
     },
-    rollupOptions: {
-      // 确保外部化处理那些你不想打包进库的依赖
-      external: ['vue'],
-      output: {
-        // 在 UMD 构建模式下为这些外部化的依赖提供一个全局变量
-        globals: {
-          vue: 'Vue',
-        },
-      },
-    },
+    rollupOptions: {},
   },
   plugins: [
     VersionGitPlugin(),
