df5cde01d5bcd353944bebcb90dfab4177fd8278
xkloveme <xkloveme@gmail.com>
2024-08-21 09:33:44 +0800

    fix(build): 修正构建过程中文件输出的错误
    解决了在构建过程中文件输出到错误目录的问题。此修复更新了`vite.config.js`中的输出目录配置，并调整了类型声明的输出目录。同时，修正了`package.json`中对主模块和类型引用的错误。

--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,30 @@
 import { resolve } from 'path'
 import VersionGitPlugin from 'vite-plugin-git-version'
 import { defineConfig } from 'vite'
-
+import dts from 'vite-plugin-dts'
 export default defineConfig({
   build: {
     outDir: 'lib',
+    minify: false,
     lib: {
-      entry: resolve(__dirname, './src/index.js'),
+      entry: resolve(__dirname, 'src/index.js'),
       name: 'jxk',
-      fileName: 'index',
+      fileName: (format) => `index.${format}.js`,
+      formats: ['es', 'cjs']
     },
-    rollupOptions: {},
+    rollupOptions: {
+      external: [], // 在这里列出外部依赖
+      output: {
+        preserveModules: true,
+        exports: 'named'
+      }
+    },
   },
   plugins: [
     VersionGitPlugin(),
+    dts({
+      insertTypesEntry: true,
+      outDir: 'lib/types'
+    })
   ],
 })
